969cf844973098f3dd9d60a82b4f1ef095ad9867
Tormente, Rodrigo <118151332+RTormente@users.noreply.github.com>
2023-12-26 03:22:11 -0300

    testes para criação de uma home responsiva

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,9 @@
 import { renderComponent } from "./components.js";
+//import { dataJson } from "./dataJson.js";
+//import SlideShow from "./slide_show.js";
 
 await renderComponent("assets/components/footer.html", "footer");
 await renderComponent("assets/components/header.html", "header");
-await renderComponent("assets/components/slideshow.html", ".slideshow");
-
 
 document.getElementById("menuMobile").addEventListener("click", (event) => {
     document.querySelector("header nav").classList.toggle("isVisibleFlex");
@@ -14,3 +14,58 @@ document.getElementById("menuClose").addEventListener("click", (event) => {
     document.querySelector("header nav").classList.toggle("isVisibleFlex");
     document.getElementById("menuClose").classList.toggle("isVisible");
 });
+
+/****** load data ******/
+
+
+/*
+const dbestabelecimentos = new dataJson("../assets/data/estabelecimento.json");
+dbestabelecimentos.consult("teste, testes, testoes, testadas");
+*/
+
+
+const estabelecimentos = [];
+const receitas = [];
+
+await fetch("../assets/data/estabelecimento.json").then((response) => {
+    response.json().then((data) => {
+        data.map((item) => {
+            estabelecimentos.push(item);
+            console.log(estabelecimentos.length);
+        });
+    });
+});
+await fetch("../assets/data/receita.json").then((response) => {
+    response.json().then((data) => {
+        data.map((item) => {
+            receitas.push(item);
+        });
+    });
+});
+
+/****** Slide Show ******/
+
+const dataList = [];
+
+for (let i = estabelecimentos.length - 2; i < estabelecimentos.length; i++) {
+    let item = {};
+    item.id = estabelecimentos[i]["idEstabelecimento"];
+    item.tipo = "estabelecimento";
+    item.imagem = estabelecimentos[i]["imagem"];
+    item.nome = estabelecimentos[i]["descricao"];
+
+    dataList.push(item);
+}
+
+for (let i = receitas.length - 2; i < receitas.length; i++) {
+    let item = {};
+    item.id = receitas[i]["idReceita"];
+    item.tipo = "receita";
+    item.imagem = receitas[i]["imagem"];
+    item.nome = receitas[i]["descricao"];
+
+    dataList.push(item);
+}
+
+
+console.log(dataList);
