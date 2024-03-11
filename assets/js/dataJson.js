34b9e57d324c2a692a4fba4efbe0893190a74962
Tormente, Rodrigo <118151332+RTormente@users.noreply.github.com>
2024-03-10 22:48:35 -0300

    criando consulta por script

--- assets/js/dataJson.js
+++ assets/js/dataJson.js
@@ -1,11 +1,35 @@
 import { dataInterface } from "./dataInterface.js";
 
-
+/*******  CLASS TO FUTURE USE  *******/
 export class dataJson extends dataInterface {
 
     validateDB(db) {
         return true
     }
     
+
+}
+
+
+/*******  FUNCTIONS TO FUTURE ADAPTATION  *******/
+export async function getJson (path) {
+    let result = [];
+
+    await fetch(path)
+        .then((response) => {
+            if (!response.ok) throw new Error('Network response error.');
+
+            return response.json();
+        })
+        .then((data) => {
+            data.map((item) => {
+                return result.push(item);
+            });
+        })
+        .catch(err => {
+            console.log('Error json: ', err.message);
+        });
+
+    return result;
 
 }
